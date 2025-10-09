b31ffaa82520817b87eb707bd30bcc5e82fd4f29
RaulChoupina <160479758+RaulChoupina@users.noreply.github.com>
2025-10-09 16:47:22 +0100

    teste

---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "DSSMV_ProjectDroid_1230593_1222100_1"
+include(":app")
